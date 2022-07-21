2a56d0b46ae7be104d1d5a9fafef0d04d0141640
GeanCarloslc <geancarloslc@icloud.com>
2022-07-20 21:07:30 -0300

    Desenvolvimento utilizando a abordagem RESTFUL

---
+++ src/main/java/io/github/geancarloslc/api/ApiErros.java
@@ -0,0 +1,18 @@
+package io.github.geancarloslc.api;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ApiErros {
+
+    @Getter
+    private List<String> errors;
+
+    public ApiErros(String mensaagemErro){
+        this.errors = Arrays.asList(mensaagemErro);
+    }
+}
